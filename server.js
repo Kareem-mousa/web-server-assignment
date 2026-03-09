32c0bea99ba03288496444e3d0eb84f3359f40dd
Kareem-mousa <s12323798@stu.najah.edu>
2026-03-09 22:15:39 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === "/") {
+
+        fs.readFile("index.htm", (err, data) => {
+
+            if (err) {
+                res.writeHead(500, {"Content-Type": "text/plain"});
+                res.end("Error loading HTML file");
+                return;
+            }
+
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(data);
+        });
+
+    }
+
+    else if (req.url === "/api") {
+
+        const student = {
+            name: "Kareem Mousa",
+            studentID: "123456"
+        };
+
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(student));
+    }
+
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
